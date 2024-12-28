12b5426cf234fc8f02dccccd3e967a6f3e1a4a62
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2024-12-28 18:54:17 +0330

    nothing happened

--- src/utils/userStore.js
+++ src/utils/userStore.js
@@ -1,13 +1,13 @@
-import {create} from "zustand";
+import { create } from "zustand";
 
 export const useUserStore = create((set) => ({
-  loggedInUser: JSON.parse(localStorage.getItem('loggedInUser')) || null,
+  loggedInUser: JSON.parse(localStorage.getItem("loggedInUser")) || null,
   setLoggedInUser: (user) => {
     set({ loggedInUser: user });
-    localStorage.setItem('loggedInUser', JSON.stringify(user));
+    localStorage.setItem("loggedInUser", JSON.stringify(user));
   },
   logoutUser: () => {
     set({ loggedInUser: null });
-    localStorage.removeItem('loggedInUser'); 
+    localStorage.removeItem("loggedInUser");
   },
 }));
