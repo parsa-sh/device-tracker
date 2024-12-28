fc4ed4d8d328c09ce095245a617152d2e15d79af
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2024-12-28 16:30:03 +0330

    Fix <routing> : Set the route  for If the user is true the main page will be home in layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,23 +5,24 @@ import Appbar from "./../components/Appbar";
 import Sidebar from "./../components/Sidebar";
 import Setting from "./../pages/Setting";
 import AddDevice from "./../pages/AddDevice";
+import { useUserStore } from "../utils/userStore";
 
 function Layout() {
+
   return (
     <Stack direction={"column"}>
-      <Appbar />
+      <Appbar/>
       <Stack direction={"row-reverse"} width={"100%"}>
         <Sidebar />
         <Box width={"100%"} height={"100vh"}>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/setting" element={<Setting />} />
             <Route path="/add-device" element={<AddDevice />} />
           </Routes>
         </Box>
       </Stack>
     </Stack>
-    
   );
 }
 
