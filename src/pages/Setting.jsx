d020583dc86e66c014149826bff4434810f1d16c
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2024-12-28 18:58:00 +0330

    Feat <page> : Create setting page elemets

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,8 +1,54 @@
+import {
+  Box,
+  FormControl,
+  FormControlLabel,
+  Switch,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+} from "@mui/material";
+import { useState } from "react";
+import BedtimeIcon from '@mui/icons-material/Bedtime';
 
 function Setting() {
+  const [lang, setLang] = useState("farsi");
+  const handleLangChange = (e) => {
+    setLang(e.target.value);
+  };
   return (
-    <div>Setting</div>
-  )
+    <Box
+      bgcolor={"#1C1C1E"}
+      width={"100%"}
+      height={"100%"}
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Stack
+        border={"2px solid grey"}
+        width={"60vw"}
+        height={"60vh"}
+        borderRadius={"12px"}
+        padding={"24px"}
+        bgcolor={"white"}
+        gap={"24px"}
+      >
+        <FormControl variant="standard">
+          <InputLabel>زبان</InputLabel>
+          <Select value={lang} onChange={handleLangChange}>
+            <MenuItem value={"farsi"}>فارسی</MenuItem>
+            <MenuItem value={"english"}>انگلیسی</MenuItem>
+          </Select>
+        </FormControl>
+        <Stack direction={"row-reverse"} justifyContent={"flex-start"} alignItems={"center"}>
+          <BedtimeIcon color="primary"/>
+          <FormControlLabel control={<Switch defaultChecked />} label="حالت شب"/>
+        </Stack>
+      </Stack>
+    </Box>
+  );
 }
 
-export default Setting
+export default Setting;
