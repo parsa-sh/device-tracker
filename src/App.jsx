44b0c2068f2991f2e6f2141e40094a9bdbbe39ac
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2024-12-28 16:28:18 +0330

    Refactor <Routing> : Set the routing for user that if user is true it redirect to home else to login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router";
 import Layout from "./layout/Layout";
 import Login from "./pages/Login";
-import { useState } from "react";
-
+import { useUserStore } from "./utils/userStore";
 
 function App() {
-  const [token , setToken] = useState(false)
-  const handleLogin = ()=>{
-    setToken(true)
-  }
+  const loggedInUser = useUserStore((state) => state.loggedInUser);
+
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login onLogin={handleLogin} />} />
-        {/* <Route path="/*" element={token ? <Layout/> : <Navigate to="/login"/>} /> */}
-        <Route path="/*" element={<Layout/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/*"
+          element={loggedInUser ? <Layout /> : <Navigate to="/login" replace />}
+        />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
