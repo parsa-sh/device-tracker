40f074859efafed485e9e97d25240d488493cfcb
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2024-12-28 16:27:03 +0330

    Fix <zustand> : Add function like refresh token that every time thebrowser refreshed the user won't logout

--- src/utils/userStore.js
+++ src/utils/userStore.js
@@ -1,7 +1,13 @@
-import create from "zustand";
+import {create} from "zustand";
 
 export const useUserStore = create((set) => ({
-  loggedInUser: null,
-  setLoggedInUser: (user) => set({ loggedInUser: user }),
-  LogoutUser: () => set({ loggedInUser: null }),
+  loggedInUser: JSON.parse(localStorage.getItem('loggedInUser')) || null,
+  setLoggedInUser: (user) => {
+    set({ loggedInUser: user });
+    localStorage.setItem('loggedInUser', JSON.stringify(user));
+  },
+  logoutUser: () => {
+    set({ loggedInUser: null });
+    localStorage.removeItem('loggedInUser'); 
+  },
 }));
