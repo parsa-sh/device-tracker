7b8747babc87870f12c6ab0860ea1a65a6f13c50
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2024-12-28 12:03:00 +0330

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./layout/Layout";
 import Login from "./pages/Login";
 import { useState } from "react";
 
+
 function App() {
   const [token , setToken] = useState(false)
   const handleLogin = ()=>{
