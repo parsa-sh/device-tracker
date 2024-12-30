9060b61b246e689f0ce1942c6f51b3ac6febfd31
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2024-12-30 10:34:07 +0330

    Fix <zustand> : Make the stored user data in session storage than local storage

--- src/utils/userStore.js
+++ src/utils/userStore.js
@@ -1,13 +1,13 @@
 import { create } from "zustand";
 
 export const useUserStore = create((set) => ({
-  loggedInUser: JSON.parse(localStorage.getItem("loggedInUser")) || null,
+  loggedInUser: JSON.parse(sessionStorage.getItem("loggedInUser")) || null,
   setLoggedInUser: (user) => {
     set({ loggedInUser: user });
-    localStorage.setItem("loggedInUser", JSON.stringify(user));
+    sessionStorage.setItem("loggedInUser", JSON.stringify(user));
   },
   logoutUser: () => {
     set({ loggedInUser: null });
-    localStorage.removeItem("loggedInUser");
+    sessionStorage.removeItem("loggedInUser");
   },
 }));
