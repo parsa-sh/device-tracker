478c6a7ad163e4e8913742a0bb36bcf49da59d34
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2025-01-04 18:28:11 +0330

    Feat <user> : Add a page for restrict normal user that can't go to user page

---
+++ src/components/RoleError.jsx
@@ -0,0 +1,33 @@
+import { Box, Typography } from "@mui/material";
+import { useThemeStore } from "../utils/userStore";
+
+function RoleError() {
+  const { theme } = useThemeStore();
+  return (
+    <Box
+      width={"100%"}
+      height={"100%"}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+      sx={
+        theme === "light"
+          ? { backgroundColor: "white" }
+          : { backgroundColor: "#1C1C1E" }
+      }
+    >
+      <Typography
+        fontSize={"68px"}
+        color={theme === "light" ? "black" : "white"}
+      >
+        Access Denied !
+      </Typography>
+      <Typography color={theme === "light" ? "black" : "white"}>
+        Only admin user can access
+      </Typography>
+    </Box>
+  );
+}
+
+export default RoleError;
