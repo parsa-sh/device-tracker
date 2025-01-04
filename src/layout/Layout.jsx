00b1ab079eb69c65e328d030bcb8f0bddd4d1076
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2025-01-04 18:28:41 +0330

    Feat <userRole> : Add logic for user roles

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -6,27 +6,33 @@ import Sidebar from "./../components/Sidebar";
 import Setting from "./../pages/Setting";
 import AddDevice from "./../pages/AddDevice";
 import User from "../pages/User";
+import RoleError from "./../components/RoleError";
 import Trucks from "../pages/Trucks";
 import Drivers from "../pages/Drivers";
+import { useUserStore } from "../utils/userStore";
 
 function Layout() {
+  const loggedInUser = useUserStore((state) => state.loggedInUser);
   return (
-      <Stack direction={"column"} maxHeight={"100%"}>
-        <Appbar />
-        <Stack direction={"row-reverse"} width={"100%"} maxHeight={"100vh"}>
-          <Sidebar />
-          <Box width={"100%"} height={"100vh"}>
-            <Routes>
-              <Route path="/home" element={<Home />} />
-              <Route path="/setting" element={<Setting />} />
-              <Route path="/devices" element={<AddDevice />} />
-              <Route path="/user" element={<User />} />
-              <Route path="/trucks" element={<Trucks />} />
-              <Route path="/drivers" element={<Drivers />} />
-            </Routes>
-          </Box>
-        </Stack>
+    <Stack direction={"column"} maxHeight={"100%"}>
+      <Appbar />
+      <Stack direction={"row-reverse"} width={"100%"} maxHeight={"100vh"}>
+        <Sidebar />
+        <Box width={"100%"} height={"100vh"}>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/setting" element={<Setting />} />
+            <Route path="/devices" element={<AddDevice />} />
+            <Route
+              path="/user"
+              element={loggedInUser.role === "admin" ? <User /> : <RoleError />}
+            />
+            <Route path="/trucks" element={<Trucks />} />
+            <Route path="/drivers" element={<Drivers />} />
+          </Routes>
+        </Box>
       </Stack>
+    </Stack>
   );
 }
 
