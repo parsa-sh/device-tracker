017d413dc0069f38c99765ffd1eef2ba370b9655
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2025-01-04 18:26:38 +0330

    Feat <page> : Add user page codes and logic

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,242 +1,129 @@
-import {
-  Button,
-  FormControl,
-  IconButton,
-  Input,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { useUserStore } from "../utils/userStore";
-import { useState } from "react";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { Avatar, Button, Chip, Stack } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import UserEdit from "../components/UserEdit";
+
 function User() {
-  const loggedInUser = useUserStore((state) => state.loggedInUser);
-  const [showPassword, setShowPassword] = useState(false);
-  const handleShowPassword = () => {
-    setShowPassword((show) => !show);
-  };
-  return (
-    <Stack
-      direction={"column"}
-      alignItems={"center"}
-      gap={"12px"}
-      justifyContent={"flex-end"}
-      width={"100%"}
-    >
-      <Stack
-        direction={"column"}
-        width={"90vw"}
-        textAlign={"right"}
-        mt={"38px"}
-        pb={"16px"}
-        gap={"12px"}
-      >
-        <Typography sx={{ fontSize: "24px", fontWeight: "700" }}>
-          حساب کاربری
-        </Typography>
-        <Typography sx={{ fontSize: "14px" }}>
-          اطلاعات و فعالیت های بلادرنگ شما
-        </Typography>
-        <div
-          style={{
-            height: "1px",
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "12px",
-          }}
-        >
-          <div
-            style={{ backgroundColor: "#c7c7c7", width: "97%", height: "100%" }}
-          ></div>
-        </div>
-      </Stack>
-      <Stack
-        direction={"row-reverse"}
-        alignItems={"center"}
-        justifyContent={"space-between"}
-        width={"97%"}
-        paddingx={"38px"}
-      >
-        <Stack
-          direction={"row-reverse"}
-          gap={"12px"}
-          justifyContent={"flex-end"}
-        >
-          <img
-            src={loggedInUser.picture}
-            style={{ width: "200px", borderRadius: "20%" }}
-            alt="profile"
-          />
-          <Stack textAlign={"right"} justifyContent={"center"} gap={"7px"}>
-            <Typography sx={{ fontWeight: "700" }}>عکس کاربری</Typography>
-            <Typography sx={{ fontSize: "12px" }}>
-              عکس به صورت jpeg ,png و کمتراز 20 مگابایت باشد
-            </Typography>
-          </Stack>
+  const [users, setUsers] = useState([]);
+  const [editPage, setEditPage] = useState(false);
+
+  const handleEdit = ()=>{
+    setEditPage(true)
+  }
+  const handleEditClose = ()=>{
+    setEditPage(false)
+  }
+
+  const columns = [
+    {
+      field: "companyCode",
+      headerName: "شماره پرسنلی",
+      width: 200,
+    },
+    {
+      field: "picture",
+      HeaderName: "تصویر کاربر",
+      width: 300,
+      renderCell: (params) => (
+        <Stack width={"100%"} height={"100%"} justifyContent={"center"}>
+          <Avatar sx={{ width: "120px", height: "120px" }} src={params.value} />
         </Stack>
+      ),
+    },
+    { field: "name", headerName: "نام و نام خانوادگی", width: 300 },
+    {
+      field: "role",
+      headerName: "نقش کاربری",
+      flex: 1,
+      renderCell: (params) => (
         <Stack
-          direction={"row-reverse"}
-          gap={"24px"}
+          width={"100%"}
           height={"100%"}
+          direction={"row"}
           justifyContent={"space-between"}
           alignItems={"center"}
         >
+          <Chip
+            sx={{
+              width: "62px",
+              height: "58px",
+              fontSize: "12px",
+              fontWeight: "600",
+              borderRadius: "50%",
+            }}
+            color="success"
+            label={params.value}
+          />
           <Button
+            color="secondary"
             variant="contained"
             disableElevation
-            sx={{
-              fontSize: "12px",
-              fontWeight: "700",
-              borderRadius: "12px",
-              paddingY: "14px",
-            }}
+            sx={{ borderRadius: "12px", fontWeight: "700" }}
+            onClick={handleEdit}
           >
-            آپلود عکس جدید
-          </Button>
-          <Button
-            variant="contained"
-            color="error"
-            disableElevation
-            sx={{
-              fontSize: "12px",
-              fontWeight: "700",
-              borderRadius: "12px",
-              paddingY: "14px",
-            }}
-          >
-            حذف عکس فعلی
+            ویرایش
           </Button>
         </Stack>
-      </Stack>
+      ),
+    },
+  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("http://localhost:7000/users");
+        const userData = res.data.map((user) => ({
+          id: user.id,
+          picture: user.picture,
+          name: user.name,
+          role: user.role,
+          companyCode: user.companyCode,
+          email: user.email,
+          password: user.password,
+        }));
+        setUsers(userData);
+      } catch (error) {
+        alert(error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <>
       <Stack
-        direction={"column"}
-        justifyContent={"space-between"}
-        alignItems={"center"}
-        gap={"38px"}
-        width={"97%"}
-        mt={"24px"}
+        sx={{ direction: "rtl" }}
+        overflow={"hidden"}
+        justifyContent={"center"}
+        padding={"12px"}
+        maxHeight={"100vh"}
       >
-        <Stack
-          direction={"row"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-          width={"100%"}
-        >
-          <TextField
-            label="کد پرسنلی"
-            value={loggedInUser.companyCode}
+        <Stack maxWidth={"100vw"} maxHeight={"100vh"}>
+          <DataGrid
+            rows={users}
+            columns={columns}
+            disableSelectionOnClick
+            disableColumnFilter={true}
+            disableColumnMenu={true}
+            disableColumnResize={true}
+            disableAutosize={true}
+            rowHeight={150}
             sx={{
-              "& .MuiFormLabel-root.MuiInputLabel-root": {
-                right: "-38px",
-                top: "-3px",
-                direction: "rtl",
+              borderRadius: "12px",
+              "& .MuiDataGrid-columnHeaders": {
+                textAlign: "right",
+                position: "sticky",
+                top: 0,
+                zIndex: 1000,
               },
-              "& .MuiInputBase-root.MuiOutlinedInput-root": {
-                direction: "rtl",
-              },
-            }}
-          />
-          <TextField
-            label="نام خانوادگی"
-            value={loggedInUser.name}
-            sx={{
-              "& .MuiFormLabel-root.MuiInputLabel-root": {
-                right: "-38px",
-                top: "-3px",
-                direction: "rtl",
-              },
-              "& .MuiInputBase-root.MuiOutlinedInput-root": {
-                direction: "rtl",
-              },
-            }}
-          />
-          <TextField
-            label="نام کاربری"
-            value={`${loggedInUser.username} @`}
-            sx={{
-              "& .MuiFormLabel-root.MuiInputLabel-root": {
-                right: "-36px",
-                top: "-3px",
-                direction: "rtl",
-              },
-              "& .MuiInputBase-root.MuiOutlinedInput-root": {
-                direction: "rtl",
-              },
-            }}
-          />
-        </Stack>
-        <Stack
-          direction={"row"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-          width={"100%"}
-        >
-          <TextField
-            label="نقش کاربری"
-            value={loggedInUser.role}
-            sx={{
-              "& .MuiFormLabel-root.MuiInputLabel-root": {
-                right: "-38px",
-                top: "-3px",
-                direction: "rtl",
-              },
-              "& .MuiInputBase-root.MuiOutlinedInput-root": {
-                direction: "rtl",
-              },
-            }}
-          />
-          <FormControl
-            variant="outlined"
-            sx={{
-              "& .MuiFormLabel-root.MuiInputLabel-root": {
-                right: "-50px",
-                top: "-3px",
-                direction: "rtl",
-              },
-              "& .MuiInputBase-root.MuiOutlinedInput-root": {
-                direction: "rtl",
-              },
-            }}
-          >
-            <InputLabel htmlFor="outlined-adornment-password">
-              کلمه عبور
-            </InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={showPassword ? "text" : "password"}
-              value={loggedInUser.password}
-              startAdornment={
-                <InputAdornment position="start">
-                  <IconButton onClick={handleShowPassword}>
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              label="password"
-            />
-          </FormControl>
-          <TextField
-            label="ایمیل"
-            value={loggedInUser.email}
-            sx={{
-              "& .MuiFormLabel-root.MuiInputLabel-root": {
-                right: "-38px",
-                top: "-3px",
-                direction: "rtl",
-              },
-              "& .MuiInputBase-root.MuiOutlinedInput-root": {
-                direction: "rtl",
+              "& .MuiDataGrid-cell": {
+                textAlign: "right",
               },
             }}
           />
         </Stack>
       </Stack>
-    </Stack>
+      {editPage && <UserEdit onClose={handleEditClose} />}
+    </>
   );
 }
 
