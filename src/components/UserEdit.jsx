37c75cf2b3e84ffdc9bdb997db45fddcca124fd7
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2025-01-05 13:23:47 +0330

    Feat <user> : Create edit page for editing user detail

--- src/components/UserEdit.jsx
+++ src/components/UserEdit.jsx
@@ -1,7 +1,18 @@
-import { Box, IconButton, Stack, Typography } from "@mui/material";
+import {
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import CancelIcon from "@mui/icons-material/Cancel";
+import { useUserStore } from "../utils/userStore";
 
-function UserEdit({onClose}) {
+function UserEdit({ onClose }) {
+  const loggedInUser = useUserStore((state) => state.loggedInUser);
   return (
     <Box
       width={"100%"}
@@ -17,15 +28,114 @@ function UserEdit({onClose}) {
     >
       <Stack
         bgcolor={"white"}
-        height={"60%"}
+        height={"65%"}
         width={"40%"}
         borderRadius={"24px"}
         position={"relative"}
+        direction={"column"}
+        px={"28px"}
+        justifyContent={"center"}
       >
-        <IconButton sx={{ position: "absolute", top: "7px", right: "7px" }} onClickCapture={onClose}>
-          <CancelIcon />
-        </IconButton>
-        <Typography>test</Typography>
+        <Stack
+          direction={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          marginTop={"14px"}
+          gap={"24px"}
+        >
+          <Avatar
+            src={loggedInUser.picture}
+            sx={{ width: "120px", height: "120px" }}
+          />
+          <Stack
+            justifyContent={"center"}
+            alignItems={"center"}
+            direction={"column"}
+            gap={"12px"}
+          >
+            <Typography fontSize={"34px"} fontWeight={"600"}>
+              {loggedInUser.name}
+            </Typography>
+          </Stack>
+        </Stack>
+        <div
+          style={{
+            height: "1px",
+            backgroundColor: "#b5b5b5",
+            width: "100%",
+            marginTop: "24px",
+          }}
+        ></div>
+        <Stack marginTop={"34px"} gap={"14px"}>
+          <Stack
+            direction={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <TextField size="small" value={loggedInUser.name} />
+            <Typography>نام و نام خانوادگی</Typography>
+          </Stack>
+          <Stack
+            direction={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <TextField size="small" value={loggedInUser.email} />
+            <Typography>ایمیل</Typography>
+          </Stack>
+          <Stack
+            direction={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <TextField size="small" value={loggedInUser.companyCode} />
+            <Typography>کد پرسنلی</Typography>
+          </Stack>
+          <Stack
+            direction={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <TextField
+              size="small"
+              value={loggedInUser.password}
+              type="password"
+            />
+            <Typography>کلمه عبور</Typography>
+          </Stack>
+          <Stack
+            direction={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <TextField size="small" />
+            <Typography>کلمه عبور جدید</Typography>
+          </Stack>
+        </Stack>
+        <Stack
+          direction={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          width={"100%"}
+          marginTop={"34px"}
+        >
+          <Button
+            variant="contained"
+            color="error"
+            sx={{ fontWeight: "700", width: "200px", height: "54px" }}
+            onClick={onClose}
+          >
+            انصراف
+          </Button>
+          <Button
+            color="success"
+            variant="contained"
+            sx={{ fontWeight: "700", width: "200px", height: "54px" }}
+            onClick={()=>alert("اطلاعان ثبت شد")}
+          >
+            ثبت
+          </Button>
+        </Stack>
       </Stack>
     </Box>
   );
