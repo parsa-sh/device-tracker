dc1b2c726de299de8160abc99b7ef5e930441036
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2025-01-06 15:21:02 +0330

    Feat <api> : Create axios configs with interceptors for refresh_token and access_token

---
+++ src/utils/api.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://192.168.88.17:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response.status === 401 &&
+      originalRequest &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      const refreshToken = localStorage.getItem("refresh");
+
+      if (refreshToken) {
+        try {
+          const res = await axios.post("/authentication/refresh/", {
+            refresh: refreshToken,
+          });
+
+          const { access } = res.data;
+
+          if (access) {
+            localStorage.setItem("access", access);
+            originalRequest.headers["Authorization"] = `Bearer ${access}`;
+            return api(originalRequest);
+          }
+        } catch (refreshError) {
+          console.error("Refresh token error:", refreshError);
+          localStorage.removeItem("access");
+          localStorage.removeItem("refresh");
+          window.location.href = "/login";
+        }
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+export default api;
