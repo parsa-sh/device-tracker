4a441369ec866d386a7527388674ec5eef8a6976
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2025-01-07 14:24:08 +0330

    Change the api from static to real time live api call and add virtual loading to grid for performance

--- src/pages/AddDevice.jsx
+++ src/pages/AddDevice.jsx
@@ -1,47 +1,46 @@
 import { useEffect, useState } from "react";
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { SimpleTreeView, TreeItem } from "@mui/x-tree-view";
-
+import { useThemeStore } from "../utils/userStore";
 import axios from "axios";
 
 function AddDevice() {
   const [row, setRow] = useState([]);
+  const { theme } = useThemeStore();
   const [loading, setLoading] = useState(true);
   const columns = [
-    { field: "id", headerName: "شناسه", width: 150 },
-    { field: "name", headerName: "نام دستگاه", width: 150 },
-    { field: "model", headerName: "مدل ماشین", width: 200 },
-    { field: "serial_number", headerName: "سریال دستگاه", width: 200 },
-    { field: "imei", headerName: "شناسه بین المللی", width: 200 },
     {
-      field: "locks",
-      headerName: "قفل ها",
-      width: 400,
-      renderCell: (params) => {
-        const locks = params.value;
-
-        if (!locks || !Array.isArray(locks)) {
-          return <span>اطلاعات قفل‌ها موجود نیست</span>;
-        }
-
-        return (
-          <SimpleTreeView
-          >
-            {locks.map((lock) => (
-              <TreeItem
-                key={lock.lock_id}
-                itemId={lock.lock_id}
-                label={`سریال: ${lock.serial_number} ---------- وضعیت: ${lock.status==="locked"?"قفل":"باز"}`}
-              />
-            ))}
-          </SimpleTreeView>
-        );
-      },
+      field: "id",
+      headerName: "شناسه",
+      width: 150,
+      headerClassName: "grid--header",
+      cellClassName: "grid--cell",
     },
-
+    {
+      field: "imei",
+      headerName: "شناسه بین المللی",
+      width: 200,
+      headerClassName: "grid--header",
+      cellClassName: "grid--cell",
+    },
+    {
+      field: "type",
+      headerName: "مدل",
+      width: 200,
+      headerClassName: "grid--header",
+      cellClassName: "grid--cell",
+    },
+    {
+      field: "company",
+      headerName: "شرکت",
+      width: 200,
+      headerClassName: "grid--header",
+      cellClassName: "grid--cell",
+    },
     {
       field: "is_active",
+      headerClassName: "grid--header",
+      cellClassName: "grid--cell",
       headerName: "فعال",
       width: 100,
       renderCell: (params) => (
@@ -64,7 +63,7 @@ function AddDevice() {
                   backgroundColor: "red",
                   textAlign: "center",
                   width: "55%",
-                  height: "80%",
+                  height: "60%",
                   borderRadius: "50%",
                   color: "white",
                   fontWeight: "600",
@@ -82,14 +81,15 @@ function AddDevice() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:7000/devices");
+        const response = await axios.get(
+          "http://192.168.88.17:8000/api/devices/"
+        );
         const formattedRows = response.data.map((r) => ({
           id: r.id,
-          name: r.name,
-          serial_number: r.serial_number,
           imei: r.imei,
+          type: r.type,
+          company: r.company,
           is_active: r.is_active,
-          locks: r.locks || [],
         }));
         setRow(formattedRows);
       } catch (error) {
@@ -101,50 +101,102 @@ function AddDevice() {
     fetchData();
   }, []);
   return (
-    <Stack
-      sx={{ direction: "rtl" }}
-      overflow={"hidden"}
-      justifyContent={"center"}
-      padding={"12px"}
-      maxHeight={"100vh"}
-    >
-      <Stack maxWidth={"90vw"} maxHeight={"85vh"}>
-        <DataGrid
-          rows={row}
-          columns={columns}
-          loading={loading}
-          disableSelectionOnClick
-          disableColumnFilter={true}
-          disableColumnMenu={true}
-          disableColumnResize={true}
-          disableAutosize={true}
-          rowHeight={80}
-          sx={{
-            borderRadius: "12px",
-            "& .MuiDataGrid-columnHeaders": {
-              position: "sticky",
-              top: 0,
-              zIndex: 1000,
-              
-            },
-            "& .MuiDataGrid-cell": {
-              display:"flex",
-              justifyContent:"flex-start",
-              alignItems:"center"
-            },
-            "& .MuiDataGrid-virtualScroller":{
-              paddingX:"24px"
-            },
-            "& .MuiDataGrid-columnHeaderTitleContainer":{
-              width:"100%",
-              display:"flex",
-              justifyContent:"space-between",
-              alignItems:"center"
+    <Box height={"100%"} bgcolor={theme === "light" ? "white" : "#1C1C1E"}>
+      <Stack
+        sx={{ direction: "rtl" }}
+        overflow={"hidden"}
+        justifyContent={"center"}
+        padding={"12px"}
+        maxHeight={"100vh"}
+      >
+        <Stack maxWidth={"90vw"} maxHeight={"85vh"}>
+          <DataGrid
+            rows={row}
+            columns={columns}
+            loading={loading}
+            disableSelectionOnClick
+            disableColumnFilter={true}
+            disableColumnMenu={true}
+            disableColumnResize={true}
+            disableAutosize={true}
+            rowHeight={80}
+            sx={
+              theme === "dark"
+                ? {
+                    borderRadius: "12px",
+                    "& .MuiDataGrid-columnHeaders": {
+                      textAlign: "right",
+                      position: "sticky",
+                      top: 0,
+                      zIndex: 1000,
+                    },
+                    "& .MuiDataGrid-cell": {
+                      textAlign: "right",
+                    },
+                    "& .grid--header": {
+                      backgroundColor: "#1C1C1E",
+                      color: "white",
+                    },
+                    "& .grid--cell": {
+                      color: "white",
+                    },
+                    "& .MuiDataGrid-filler": {
+                      background: "#1C1C1E",
+                    },
+                    "& .MuiTablePagination-displayedRows": {
+                      visibility: "hidden",
+                    },
+                    "& .MuiTablePagination-actions": {
+                      display: "flex",
+                      flexDirection: "row-reverse",
+                      "& .MuiSvgIcon-root": {
+                        fontSize: "24px",
+                        background: "#e8e8e8",
+                        borderRadius: "50%",
+                      },
+                    },
+
+                    "& .MuiToolbar-root": {
+                      paddingY: "20px",
+                    },
+                  }
+                : {
+                    borderRadius: "12px",
+                    "& .MuiDataGrid-columnHeaders": {
+                      textAlign: "right",
+                      position: "sticky",
+                      top: 0,
+                      zIndex: 1000,
+                    },
+                    "& .MuiDataGrid-cell": {
+                      textAlign: "right",
+                    },
+                    "& .grid--header": {
+                      backgroundColor: "white",
+                      color: "black",
+                    },
+                    "& .MuiTablePagination-displayedRows": {
+                      visibility: "hidden",
+                    },
+                    "& .MuiTablePagination-actions": {
+                      display: "flex",
+                      flexDirection: "row-reverse",
+                      "& .MuiSvgIcon-root": {
+                        fontSize: "24px",
+                        background: "#e8e8e8",
+                        borderRadius: "50%",
+                      },
+                    },
+
+                    "& .MuiToolbar-root": {
+                      paddingY: "20px",
+                    },
+                  }
             }
-          }}
-        />
+          />
+        </Stack>
       </Stack>
-    </Stack>
+    </Box>
   );
 }
 
