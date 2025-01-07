8d0b33bd5849cea70f5c06153ca3dfb6e87a1e63
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2025-01-07 13:43:26 +0330

    Fix <theme> : Change the page color for dark and light

--- src/pages/Trucks.jsx
+++ src/pages/Trucks.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import axios from "axios";
+import { useThemeStore } from "../utils/userStore";
 
 function Trucks() {
   const [row, setRow] = useState([]);
+  const { theme } = useThemeStore();
   const [rowCount, setRowCount] = useState(0);
   const [paginationModel, setPaginationModel] = useState({
     page: 0,
@@ -12,11 +14,31 @@ function Trucks() {
   });
   const [loading, setLoading] = useState(true);
   const columns = [
-    { field: "id", headerName: "شناسه", width: 100 },
-    { field: "licensePlate", headerName: "شماره پلاک", width: 250 },
-    { field: "model", headerName: "مدل ماشین", width: 250 },
+    {
+      field: "id",
+      headerName: "شناسه",
+      width: 100,
+      headerClassName: "grid--header",
+      cellClassName: "grid--cell",
+    },
+    {
+      field: "licensePlate",
+      headerName: "شماره پلاک",
+      width: 250,
+      headerClassName: "grid--header",
+      cellClassName: "grid--cell",
+    },
+    {
+      field: "model",
+      headerName: "مدل ماشین",
+      width: 250,
+      headerClassName: "grid--header",
+      cellClassName: "grid--cell",
+    },
     {
       field: "is_active",
+      headerClassName: "grid--header",
+      cellClassName: "grid--cell",
       headerName: "فعال",
       width: 100,
       renderCell: (params) => (
@@ -89,50 +111,108 @@ function Trucks() {
   };
 
   return (
-    <Stack
-      sx={{ direction: "rtl" }}
-      overflow={"hidden"}
-      justifyContent={"center"}
-      padding={"12px"}
-      maxHeight={"100vh"}
-    >
-      <Stack maxWidth={"100vw"} maxHeight={"100vh"}>
-        <DataGrid
-          pagination
-          rows={row}
-          rowCount={rowCount}
-          columns={columns}
-          loading={loading}
-          disableSelectionOnClick
-          disableColumnFilter={true}
-          disableColumnMenu={true}
-          disableColumnResize={true}
-          disableAutosize={true}
-          paginationMode="server"
-          paginationModel={paginationModel}
-          onPaginationModelChange={handlePaginationChange}
-          sx={{
-            borderRadius: "12px",
-            "& .MuiDataGrid-columnHeaders": {
-              textAlign: "right",
-              position: "sticky",
-              top: 0,
-              zIndex: 1000,
-            },
-            "& .MuiDataGrid-cell": {
-              textAlign: "right",
-            },
-            "& .MuiTablePagination-displayedRows":{
-              visibility:"hidden"
-            },
-            "& .MuiTablePagination-actions":{
-              display:"flex",
-              flexDirection:"row-reverse"
+    <Box height={"100%"} bgcolor={theme === "light" ? "white" : "#1C1C1E"}>
+      <Stack
+        sx={{ direction: "rtl" }}
+        overflow={"hidden"}
+        justifyContent={"center"}
+        padding={"12px"}
+        maxHeight={"100vh"}
+      >
+        <Stack maxWidth={"100vw"} maxHeight={"100vh"}>
+          <DataGrid
+            pagination
+            rows={row}
+            rowCount={rowCount}
+            columns={columns}
+            loading={loading}
+            disableSelectionOnClick
+            disableColumnFilter={true}
+            disableColumnMenu={true}
+            disableColumnResize={true}
+            disableAutosize={true}
+            paginationMode="server"
+            paginationModel={paginationModel}
+            onPaginationModelChange={handlePaginationChange}
+            sx={
+              theme === "dark"
+                ? {
+                    borderRadius: "12px",
+                    "& .MuiDataGrid-columnHeaders": {
+                      textAlign: "right",
+                      position: "sticky",
+                      top: 0,
+                      zIndex: 1000,
+                    },
+                    "& .MuiDataGrid-cell": {
+                      textAlign: "right",
+                    },
+                    "& .grid--header": {
+                      backgroundColor: "#1C1C1E",
+                      color: "white",
+                    },
+                    "& .grid--cell": {
+                      color: "white",
+                    },
+                    "& .MuiDataGrid-filler":{
+                      background:"#1C1C1E"
+                    },
+                    "& .MuiTablePagination-displayedRows":{
+                      visibility:"hidden"
+                    },
+                    "& .MuiTablePagination-actions":{
+                      display:"flex",
+                      flexDirection:"row-reverse",
+                      "& .MuiSvgIcon-root":{
+                      fontSize:"24px",
+                      background:"#e8e8e8",
+                      borderRadius:"50%"
+                    },
+                    },
+                    
+                    "& .MuiToolbar-root":{
+                      paddingY:"20px"
+                    },
+                    
+                  }
+                : {
+                    borderRadius: "12px",
+                    "& .MuiDataGrid-columnHeaders": {
+                      textAlign: "right",
+                      position: "sticky",
+                      top: 0,
+                      zIndex: 1000,
+                    },
+                    "& .MuiDataGrid-cell": {
+                      textAlign: "right",
+                    },
+                    "& .grid--header": {
+                      backgroundColor: "white",
+                      color: "black",
+                    },
+                    "& .MuiTablePagination-displayedRows":{
+                      visibility:"hidden"
+                    },
+                    "& .MuiTablePagination-actions":{
+                      display:"flex",
+                      flexDirection:"row-reverse",
+                      "& .MuiSvgIcon-root":{
+                      fontSize:"24px",
+                      background:"#e8e8e8",
+                      borderRadius:"50%"
+                    },
+                    },
+                    
+                    "& .MuiToolbar-root":{
+                      paddingY:"20px",
+                      
+                    },
+                  }
             }
-          }}
-        />
+          />
+        </Stack>
       </Stack>
-    </Stack>
+    </Box>
   );
 }
 export default Trucks;
