721171b6549b9e8419da471c829610451544c906
Parsa Shahsavan <shahsavanparsa4@gmail.com>
2025-01-12 12:30:03 +0330

    Feat <map> : Make main map markers clickable

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,27 +1,34 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet";
 import { useCardData } from "../utils/userStore";
 
 const Map = () => {
-  const { cards, selectedMarker, selectedCard } = useCardData();
-
+  const { cards, selectedMarker, selectedCard,setSelectedMarker , setSelectedCard} = useCardData();
+  const handleCardClick = (card) => {
+    setSelectedCard(card);
+    setSelectedMarker([card]);
+  };
   return (
     <MapContainer
       center={[32.648487094050125, 51.67227251599078]}
       zoom={1}
       scrollWheelZoom={true}
-      style={{ width: "100%", height: "90vh" }}
+      style={{ width: "100%", height: "100vh" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
       {selectedCard
         ? selectedMarker?.map((e) => (
-            <Marker key={e.id} position={[e.long, e.lat]}>
-              <Popup>{e.company}</Popup>
+            <Marker
+              key={e.id}
+              position={[e.long, e.lat]}
+              
+            >
+              <Tooltip>{e.company}</Tooltip>
             </Marker>
           ))
         : cards?.map((e) => (
-            <Marker key={e.id} position={[e.long, e.lat]}>
-              <Popup>{e.company}</Popup>
+            <Marker key={e.id} position={[e.long, e.lat]} eventHandlers={{ click: () => handleCardClick(e) }}>
+              <Tooltip>{e.company}</Tooltip>
             </Marker>
           ))}
     </MapContainer>
